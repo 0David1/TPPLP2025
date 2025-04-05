557f29830946e96ea8d6ffcad9d319d02580aec6
Enzo <enzobautista1000@gmail.com>
2025-04-05 15:02:15 -0300

    ej 1-9

--- src/Documento.hs
+++ src/Documento.hs
@@ -29,21 +29,53 @@ texto [] = Vacio
 texto t = Texto t Vacio
 
 -- foldDoc :: ... PENDIENTE: Ejercicio 1 ...
-foldDoc ::
-foldDoc = error "PENDIENTE: Ejercicio 1"
+foldDoc :: b -> (String -> b -> b) -> (Int -> b -> b) -> Doc -> b
+foldDoc cVacio cTexto cLinea documento = case documento of
+    Vacio -> cVacio
+    Texto text doc ->  cTexto text (rec doc)
+    Linea n doc -> cLinea n (rec doc)
+  where
+    rec = foldDoc cVacio cTexto cLinea 
 
 -- NOTA: Se declara `infixr 6 <+>` para que `d1 <+> d2 <+> d3` sea equivalente a `d1 <+> (d2 <+> d3)`
 -- También permite que expresiones como `texto "a" <+> linea <+> texto "c"` sean válidas sin la necesidad de usar paréntesis.
 infixr 6 <+>
 
+-- funcion auxiliar para manejar el caso en que se combinen dos textos
+combinar :: String -> Doc -> Doc
+combinar s1 d2 = case d2 of
+  Vacio -> texto s1
+  Texto s2 doc -> Texto (s1++s2) doc
+  Linea n doc -> Texto s1 (Linea n doc)
+
+
 (<+>) :: Doc -> Doc -> Doc
-d1 <+> d2 = error "PENDIENTE: Ejercicio 2"
+d1 <+> d2 = foldDoc d2 combinar Linea d1
+
+-- Sea Texto s d entonces:
+
+-- s no debe ser el string vacio, se satisface pues sea `texto s` si s es el string vacio entonces la funcion texto retorna Vacio
+-- por lo que es imposible obtener un Doc de tipo `Texto s d`con s siendo ""
+
+-- s no debe contener saltos de linea, se satisface pues la funcion texto usa elem para buscar saltos de linea en el string s, en caso de haberlos retorna error
+
+-- d debe ser Vacio o Linea i d',
+
+-- sea Linea i d entonces: i >= 0,
+
+agregarIdentacion :: Int -> Int -> Doc -> Doc
+agregarIdentacion agregar n = Linea (n+agregar)
 
 indentar :: Int -> Doc -> Doc
-indentar i = error "PENDIENTE: Ejercicio 3"
+indentar i = foldDoc Vacio Texto (agregarIdentacion i) 
+
+-- 
+
+generarEspacio :: Int -> String -> String
+generarEspacio n s = "\n" ++ replicate n ' ' ++ s
 
 mostrar :: Doc -> String
-mostrar = error "PENDIENTE: Ejercicio 4"
+mostrar = foldDoc [] (++) generarEspacio
 
 -- | Función dada que imprime un documento en pantalla
 
--- src/PPON.hs
+++ src/PPON.hs
@@ -8,14 +8,21 @@ data PPON
   | ObjetoPP [(String, PPON)]
   deriving (Eq, Show)
 
+pericles1 = ObjetoPP [("nombre", TextoPP "Pericles"), ("edad", IntPP 30)]
+merlina1 = ObjetoPP [("nombre", TextoPP "Merlina"), ("edad", IntPP 24)]
+addams1 = ObjetoPP [("0", pericles1), ("1", merlina1)]
+
 pponAtomico :: PPON -> Bool
-pponAtomico = error "PENDIENTE: Ejercicio 5"
+pponAtomico p = case p of
+  TextoPP _ -> True
+  IntPP _ -> True
+  ObjetoPP _ -> False
 
 pponObjetoSimple :: PPON -> Bool
-pponObjetoSimple = error "PENDIENTE: Ejercicio 6"
+pponObjetoSimple (ObjetoPP l) = foldr (\(s, p) d -> pponAtomico p && d) True l
 
 intercalar :: Doc -> [Doc] -> Doc
-intercalar = error "PENDIENTE: Ejercicio 7"
+intercalar separador = foldr (\d ds -> if ds /= vacio then d <+> separador <+> ds else d) vacio
 
 entreLlaves :: [Doc] -> Doc
 entreLlaves [] = texto "{ }"
@@ -30,7 +37,18 @@ entreLlaves ds =
     <+> texto "}"
 
 aplanar :: Doc -> Doc
-aplanar = error "PENDIENTE: Ejercicio 8"
+aplanar = foldDoc vacio (\s d -> texto s <+> d) (\_ d -> texto " " <+> d)
 
 pponADoc :: PPON -> Doc
-pponADoc = error "PENDIENTE: Ejercicio 9"
+pponADoc p = case p of
+  TextoPP s -> texto (show s)
+  IntPP n -> texto (show n)
+  ObjetoPP l -> if not (pponObjetoSimple (ObjetoPP l)) then entreLlaves (formatoListaDoc l) 
+                                                       else aplanar (entreLlaves (formatoListaDoc l))
+  where formatoListaDoc :: (Show a) => [(a, PPON)] -> [Doc]
+        formatoListaDoc = map (\(x, y) -> texto (show x ++ ": ") <+> pponADoc y)
+
+-- Es recursion estructural pues los casos base devuelven un valor fijo que no depende de la funcion pponADoc
+-- y el caso recursivo no usa pponAdoc ni los 'y' 
+-- ('y' representa a los PPON en cada tupla de la lista que vendrian siendo las subestructuras sobre las cuales se hace la recursion)
+-- en otro lado salvo en la expresion (pponADoc y)
