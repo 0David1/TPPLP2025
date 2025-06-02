a38866bd385c3f1639099464b9c0e25d569635b3
0David1 <enzobautista1000@gmail.com>
2025-06-02 19:04:19 -0300

    last push

--- src/PPON.hs
+++ src/PPON.hs
@@ -45,13 +45,24 @@ aplanar = texto . foldDoc "" (++) (\_ d -> ' ' : d)
 
 --EJ9
 pponADoc :: PPON -> Doc
-pponADoc p = case p of
-  TextoPP s -> texto (show s)
-  IntPP n -> texto (show n)
-  ObjetoPP lista -> if pponObjetoSimple (ObjetoPP lista) then aplanar (formatoLlaves lista)
-                                                       else formatoLlaves lista
+pponADoc (TextoPP s) = texto (show s)
+pponADoc (IntPP n) = texto (show n)
+pponADoc (ObjetoPP lista) | pponObjetoSimple(ObjetoPP lista) = aplanar (formatoLlaves lista)
+                          | otherwise = formatoLlaves lista
   where formatoLlaves = entreLlaves . map (\(x, rec) -> texto (show x ++ ": ") <+> pponADoc rec)
 
--- Es recursion global pues:
--- se realizan otros llamados recursivos, en pponObjetoSimple se utiliza foldr en el caso que 
--- reciba un ObjetoPP, y claramente recibe un ObjetoPP en la guarda del if
+{-
+  Es recursion primitiva ya que accedemos y predicamos sobre la subestructura, en otra funcion ademas de pponADoc termino recursivo sin ser la funcion recursiva.
+-}
+{-
+  Es recursion primitiva pues:
+    1. Los casos base devuelven valores fijos:
+    2. el caso recursivo usa (g xs) y pponObjetoSimple en su definicion usa xs para pponAtomico que no es un llamado recursivo
+    
+    2. el caso recursivo usa (pponADoc sobre el recursivo) , y predica sobre Lista y objetoPP lista en varias partes 
+    pponObjeto lista 
+    iterativa no es porque no tiene acumulador.(el resultado recursivo es parametro) foldl
+    primitiva porque devuelve el acumulador sin usarlo como paramtetro.
+
+    el caso lo usas como parametro,   
+-}
